32d650d439834b404cfc263540ae0ee0a01b3ae2
Vinayak <vinugupta167@gmail.com>
2025-05-03 22:54:29 +0530

    first Commit StudyNotion

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses : [
+            {
+                heading : "Learn HTML",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn CSS",
+                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn JavaScript",
+                description : "JavaScript is a programming language that enables interactive web development, user interactions, and server communication efficiently",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+        ]
+    },
+    {
+        tag: 'New to coding',
+        courses : [
+            {
+                heading : "Learn Java",
+                description : "Java is a powerful, object-oriented programming language used for building web applications and backend systems with platform independence.",
+                level : 'Beginner',
+                lessionNumber : 15
+            },
+            {
+                heading : "Learn Python",
+                description : "Python is a versatile, high-level programming language known for readability, used in web development, data science, automation, machine learning, and software development.",
+                level : 'Beginner',
+                lessionNumber : 10
+            },
+            {
+                heading : "Learn C++ ",
+                description :  "C++ is a powerful, object-oriented programming language used for system programming, game development, competitive coding, software engineering, and high-performance applications.",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+        ]
+    },
+    {
+        tag: 'Most popular',
+        courses : [
+            {
+                heading : "Learn React",
+                description : "React is a JavaScript library for building user interfaces, using components, virtual DOM, and state management to create dynamic web applications.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn Node.JS",
+                description : "Node.js is a runtime environment that executes JavaScript on the server, enabling scalable, fast, and efficient backend development using event-driven architecture.",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+            {
+                heading : "Learn MongoDB",
+                description : "MongoDB is a NoSQL database that stores data in flexible, JSON-like documents, enabling scalability, high performance, and efficient handling of large datasets.",
+                level : 'Beginner',
+                lessionNumber : 4
+            },
+        ]
+    },
+    {
+        tag: 'Skills paths',
+        courses : [
+            {
+                heading : "Learn SQL",
+                description : "SQL (Structured Query Language) is used to manage, query, and manipulate relational databases, ensuring efficient data storage, retrieval, and integrity.",
+                level : 'Beginner',
+                lessionNumber : 5
+            },
+            {
+                heading : "Django",
+                description : "Django is a high-level Python web framework that promotes rapid development, scalability, security, and clean architecture for building robust web applications.",
+                level : 'Beginner',
+                lessionNumber : 10
+            },
+            {
+                heading : "Learn React Native",
+                description : "React Native is a JavaScript framework for building cross-platform mobile applications using React, enabling native-like performance and reusable components.",
+                level : 'Beginner',
+                lessionNumber : 8
+            },
+        ]
+    },
+    {
+        tag: 'Career paths',
+        courses : [
+            {
+                heading : "Learn Next.JS",
+                description : "Next.js is a React framework for building fast, server-rendered web applications with features like static generation, API routes, and performance optimization.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+            {
+                heading : "Learn TypeScript",
+                description : "TypeScript is a superset of JavaScript that adds static typing, improving code quality, scalability, and maintainability for large-scale web and backend applications.",
+                level : 'Beginner',
+                lessionNumber : 7
+            },
+            {
+                heading : "Learn Tailwind CSS",
+                description : "Tailwind CSS is a utility-first framework that simplifies styling by providing pre-defined classes, enabling fast, responsive, and customizable web design.",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
